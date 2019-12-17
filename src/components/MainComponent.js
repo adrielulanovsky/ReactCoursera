35fce8a53583add076a5b011d5d13339efd89449
Adriel Ulanovsky <adrielulanovsky@gmail.com>
2019-12-17 17:22:37 -0300

    SPA part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,7 @@ import { LEADERS } from '../shared/leaders';
 import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import DishDetail from './DishdetailComponent';
 import { Switch, Route, Redirect } from 'react-router-dom'
 
 class Main extends React.Component {
@@ -33,6 +34,13 @@ class Main extends React.Component {
       );
     }
     
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+    
     return (
       <div>
         <Header />
@@ -40,6 +48,7 @@ class Main extends React.Component {
         <Route path="/home" component={HomePage} />
         <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
         <Route exact path="/contactus" component={Contact}/>} />
+        <Route path='/menu/:dishId' component={DishWithId} />
         <Redirect to="/home" />
         </Switch>
         <Footer />
